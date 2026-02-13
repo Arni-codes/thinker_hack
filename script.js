dc6250fa5243d81ce72bdd69990ad147adad1d80
K R Kavana <krkavana09@gmail.com>
2026-02-13 22:13:47 +0530

    added trigger arrays

---
+++ script.js
@@ -0,0 +1,8 @@
+const urgency=["urgennt","immediately","now","last chance","final warning","act fast","limited time","don't miss out","urgent action required","time-sensitive","suspended"];
+const fear=["legal action","blocked","penalty","suspend","threat","danger","risk","warning","alert"];
+const reward=["free","bonus","gift","win","prize","exclusive offer","limited time offer","discount","deal","special promotion","claim","cashback"];
+const sensitive=["password","account","social security number","credit card","bank account","personal information","confidential","private","sensitive data","otp","bank","verify","pin","upi","aadhar","pan","cvv"];
+
+document.getElementById("analyzeBtn").addEventListener("click", analyze);
+document.getElementById("clearBtn").addEventListener("click", clearText);
+
